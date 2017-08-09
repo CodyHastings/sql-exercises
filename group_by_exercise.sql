c2d38309908490a58c8df1bee8b23e941843bdd4
Cody <cody@hastings.email>
2017-08-09 09:46:40 -0500

    completed group by exercise

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -13,3 +13,8 @@ Select  * from employees
 where last_name like 'E%'
 and last_name like '%E'
 group by first_name, last_name;
+
+Select * from employees 
+where last_name like '%q%'
+and last_name not like '%qu%'
+group by last_name;
